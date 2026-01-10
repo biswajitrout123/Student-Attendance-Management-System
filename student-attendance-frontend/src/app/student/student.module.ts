6ecdbddb436e590ae9b443624539d7b39389f568
Sameer2005-dot <jena80225@gmail.com>
2026-01-10 22:36:39 +0530

    Added student module

---
+++ student-attendance-frontend/src/app/student/student.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StudentRoutingModule } from './student-routing.module';
+
+@NgModule({
+  imports: [CommonModule, StudentRoutingModule],
+})
+export class StudentModule {}
